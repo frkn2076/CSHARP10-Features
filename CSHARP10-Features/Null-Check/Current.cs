e437e1c9ecc4048725e6cf003e0055b278ace208
Furkan Öztürk <ozturkfurkan1994@hotmail.com>
2021-10-25 19:38:23 +0300

    null check feature sample added

---
+++ CSHARP10-Features/Null-Check/Current.cs
@@ -0,0 +1,8 @@
+﻿namespace CSHARP10_Features.Null_Check;
+class Current
+{
+    void Sample(string input)
+    {
+        System.ArgumentNullException.ThrowIfNull(input, nameof(input));
+    }
+}
---
+++ CSHARP10-Features/Null-Check/Previous.cs
@@ -0,0 +1,11 @@
+﻿namespace CSHARP10_Features.Null_Check;
+class Previous
+{
+    void Sample(string input)
+    {
+        if (input == null)
+        {
+            throw new System.ArgumentNullException(nameof(input));
+        }
+    }
+}
